78c2aa5184b168cff6e1715f3f0c82e00811a572
Bhui <Bhui@KIRBYZ240W10-2.ad.nchecr.unsw.edu.au>
2019-02-01 16:34:56 +1100

    Change to use Java 8+ notation for file zipping

--- src/util/FileZipper.java
+++ src/util/FileZipper.java
@@ -1,6 +1,9 @@
 package util;
 
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 import java.util.zip.ZipOutputStream;
@@ -9,10 +12,11 @@ import java.util.zip.ZipOutputStream;
  * A utility class to handle zipping and unzipping of file
  *
  * @author Ben Hui
+ * @version 20190201 - Change to use Java 8+ notation for file zipping
  */
 public class FileZipper {
 
-    public static File unzipFile(File zipFile, File baseDir) 
+    public static File unzipFile(File zipFile, File baseDir)
             throws FileNotFoundException, IOException {
         FileOutputStream fos;
         BufferedOutputStream dest;
@@ -34,34 +38,62 @@ public class FileZipper {
             }
         }
         return objFile;
-    }       
-    
-    public static void zipFile(File srcFile, File tarFile) 
-            throws FileNotFoundException, IOException{
-        final int BUFFER = 2048;
-        byte[] data = new byte[BUFFER];    
-        ZipOutputStream zout;
-        ZipEntry ent;                  
-        BufferedInputStream srcBufStr;    
-       
-        int count;
-        
-        if(!tarFile.getName().endsWith(".zip")){
+    }
+
+    public static void zipFile(File srcFile, File tarFile)
+            throws FileNotFoundException, IOException {
+
+        if (!tarFile.getName().endsWith(".zip")) {
             tarFile.renameTo(new File(tarFile.getAbsolutePath() + ".zip"));
-        }        
-        
-        zout = new ZipOutputStream(new FileOutputStream(tarFile));
-        srcBufStr = new BufferedInputStream(new FileInputStream(srcFile), BUFFER);        
-        
-        ent = new ZipEntry(srcFile.getName());
-        zout.putNextEntry(ent);
-        
-        while((count = srcBufStr.read(data,0,BUFFER)) != -1){
-            zout.write(data, 0, count);
         }
-        
-        srcBufStr.close();
-        zout.close();               
-        
+
+        if (srcFile.isDirectory()) {
+            Path p = Files.createFile(tarFile.toPath());
+
+            try (ZipOutputStream zs = new ZipOutputStream(Files.newOutputStream(p))) {
+                Path pp = srcFile.toPath();
+                Files.walk(pp)
+                        .filter(path -> !Files.isDirectory(path))
+                        .forEach(path -> {
+                            ZipEntry zipEntry = new ZipEntry(pp.relativize(path).toString());
+                            try {
+                                zs.putNextEntry(zipEntry);
+                                Files.copy(path, zs);
+                                zs.closeEntry();
+                            } catch (IOException e) {
+                                System.err.println(e);
+                            }
+                        });
+            }
+        } else {
+            
+            // Original method
+
+            final int BUFFER = 2048;
+            byte[] data = new byte[BUFFER];
+            ZipOutputStream zout;
+            ZipEntry ent;
+            BufferedInputStream srcBufStr;
+
+            int count;
+
+            if (!tarFile.getName().endsWith(".zip")) {
+                tarFile.renameTo(new File(tarFile.getAbsolutePath() + ".zip"));
+            }
+
+            zout = new ZipOutputStream(new FileOutputStream(tarFile));
+            srcBufStr = new BufferedInputStream(new FileInputStream(srcFile), BUFFER);
+
+            ent = new ZipEntry(srcFile.getName());
+            zout.putNextEntry(ent);
+
+            while ((count = srcBufStr.read(data, 0, BUFFER)) != -1) {
+                zout.write(data, 0, count);
+            }
+
+            srcBufStr.close();
+            zout.close();
+
+        }
     }
 }
